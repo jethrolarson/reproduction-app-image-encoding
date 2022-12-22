418e9f9d4fa84c0e97c0bc541d97b35957d41238
Jethro Larson <jethro.larson@tableau.com>
2022-12-22 15:08:36 -0800

    added repro for image src encoding issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return <Image src="/Hierarchy Filter v05.svg" width={50} height={50} alt="" />
 }
